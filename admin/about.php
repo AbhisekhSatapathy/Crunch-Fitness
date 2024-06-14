b9db253d8bc72af8ce6a9b9752ce411ac2eaef92
Abhisekh Satapathy <149135663+AbhisekhSatapathy@users.noreply.github.com>
2024-06-14 11:39:32 +0530

    Add files via upload

---
+++ admin/about.php
@@ -0,0 +1,49 @@
+<?php include('../constant/layout/head.php');?>
+<?php include('../constant/layout/header.php');?>
+<?php include('../constant/layout/sidebar.php');
+?>
+ 
+        <div class="page-wrapper">
+            
+            <div class="row page-titles">
+                <div class="col-md-5 align-self-center">
+                    <h3 class="text-primary">Add Brand Management</h3> </div>
+                <div class="col-md-7 align-self-center">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+                        <li class="breadcrumb-item active">Add Brand</li>
+                    </ol>
+                </div>
+            </div>
+            
+            
+            <div class="container-fluid">
+                
+                
+                
+                <div class="row">
+                    <div class="col-lg-8" style="    margin-left: 10%;">
+                        <section class="card">
+<div class="card-header user-header alt bg-dark">
+<div class="media">
+<a href="#">
+<img class="align-self-center rounded-circle mr-3" style="width:85px; height:85px;" alt="" src="assets/uploadImage/Logo/favicon.png">
+</a>
+<div class="media-body">
+<h2 class="text-light display-6">Author Name : Mayuri K</h2>
+<p style="color: white">Freelancer - PHP, Codeignitor & Laravel</p>
+</div>
+</div>
+</div>
+<
+</section>
+                    </div>
+                  
+                </div>
+                
+               
+
+
+<?php include('./constant/layout/footer.php');?>
+
+
